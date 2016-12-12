56ebe560a778c46ebe65274588c354ecb181a1c3
Rick Mark <rickmark@outlook.com>
2016-12-11 23:16:26 -0500

    Made requested changes for style

--- Formula/logstash.rb
+++ Formula/logstash.rb
@@ -42,31 +42,31 @@ class Logstash < Formula
   plist_options :manual => "logstash"
 
   def plist; <<-EOS.undent
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <false/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/logstash</string>
+        </array>
+        <key>EnvironmentVariables</key>
         <dict>
-          <key>KeepAlive</key>
-          <false/>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{HOMEBREW_PREFIX}/bin/logstash</string>
-          </array>
-          <key>EnvironmentVariables</key>
-          <dict>
-          </dict>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>WorkingDirectory</key>
-          <string>#{var}</string>
-          <key>StandardErrorPath</key>
-          <string>#{var}/log/logstash.log</string>
-          <key>StandardOutPath</key>
-          <string>#{var}/log/logstash.log</string>
         </dict>
-      </plist>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}/logstash</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/logstash.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/logstash.log</string>
+      </dict>
+    </plist>
     EOS
   end
 
